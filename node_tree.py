bda0ac94805861709eac35fa30db1946f118a4c4
Richard Li <Richard.Li@akqa.com>
2015-07-28 10:47:15 +0800

    2-3 node balanced tree

--- node_tree.py
+++ node_tree.py
@@ -23,36 +23,17 @@ class Node:
                     self.left = node.left
                     self.middle = node.middle
 
-            if node is None:
-                return None
+            def add_node(branch, node_splitter):
+                return self.insert(branch.insert(node, is_down), False) if branch is not None and is_down \
+                    else add_value() if self.key2 is None else node_splitter()
 
-            if node.key1 > self.key1:
-                if self.left is not None and is_down:
-                    return self.insert(self.left.insert(node, is_down), False)
-                elif self.key2 is None:
-                    add_value()
-                    return None
-                else:
-                    return Node(self.key1, node, Node(self.key2, self.middle, self.right))
-            elif self.key1 >= node.key1 > self.key2:
-                if self.middle is not None and is_down:
-                    return self.insert(self.middle.insert(node, is_down), False)
-                elif self.key2 is None:
-                    add_value()
-                    return None
-                else:
-                    return Node(node.key1, Node(self.key1, self.left, node.left), Node(self.key2, node.right, self.right))
-            elif node.key1 <= self.key2:
-                if self.right is not None and is_down:
-                    return self.insert(self.right.insert(node, is_down), False)
-                elif self.key2 is None:
-                    add_value()
-                    return None
-                else:
-                    return Node(self.key2, Node(self.key1, self.left, self.middle), node)
+            return None if node is None \
+                else add_node(self.left, lambda: Node(self.key1, node, Node(self.key2, self.middle, self.right))) if node.key1 > self.key1 \
+                else add_node(self.middle, lambda: Node(node.key1, Node(self.key1, self.left, node.left), Node(self.key2, node.right, self.right))) if self.key1 >= node.key1 > self.key2 \
+                else add_node(self.right, lambda: Node(self.key2, Node(self.key1, self.left, self.middle), node))
 
     def __repr__(self):
-        return "{%s}[%s]{%s}[%s]{%s}" % (self.left, self.key1, self.middle, self.key2, self.right)
+        return "[%s] [%s]" % (self.key1, self.key2) + "\n{%s} {%s} {%s}\n" % (self.left, self.middle, self.right)
 
 n = Node(300)
 n = n.insert(Node(400)) or n
@@ -60,7 +41,11 @@ n = n.insert(Node(350)) or n
 n = n.insert(Node(200)) or n
 n = n.insert(Node(100)) or n
 n = n.insert(Node(250)) or n
+n = n.insert(Node(50)) or n
+n = n.insert(Node(220)) or n
+n = n.insert(Node(310)) or n
 print(n)
+
 
 
 
